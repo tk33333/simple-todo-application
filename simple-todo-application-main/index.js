8432fd2b605d78d13dfdef26bc94934c52d214d5
a5822047 <a5822047@aoyama.jp>
2023-06-23 16:22:29 +0900

    メッセージ

--- simple-todo-application-main/index.js
+++ simple-todo-application-main/index.js
@@ -43,8 +43,11 @@ function onUpdateIsDone(task) {
     console.log("チェックボックスがクリックされました。", task);
 
     // 達成状態を更新(このままだと、現在の達成状態で更新されてしまう！)
-    const currentValue = task.isDone;
+    a=task.isDone;
     task.isDone = currentValue;
+    currentValue=task.isDone;
+
+
 
     return task;
 }
